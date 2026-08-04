6bdaafda27ef75c84fe079604311ec7dbd20ef34
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2026-08-04 12:09:11 -0300

    subindo atualizaçoes para correção do arquivo workflows .8

--- test/pageobjects/cadastro.page.js
+++ test/pageobjects/cadastro.page.js
@@ -2,52 +2,35 @@ import { $ } from '@wdio/globals'
 
 class CadastroPage {
     // 1. Mapeamento dos elementos
+    get tabProfile() {
+        return $('// *[@resource-id="tab-profile"]'); // Mapeia a aba perfil
+    }
+
     get btnsignUp() {
         return $('id:signUp')
     }
 
-    get firstName() {
-        return $('id:firstName')
-    }
-
-    get lastName() {
-        return $('id:lastName')
-    }
-
-    get phoneNumber() {
-        return $('id:phone')
-    }
-
-    get email() {
-        return $('id:email')
-    }
-
-    get password() {
-        return $('id:password')
-    }
-
-    get repassword() {
-        return $('id:repassword')
-    }
-
-    get btnCreate() {
-        return $('id:create')
-    }
-
-    get btnCreateWishlist() {
-        return $('id:wishlist')
-    }
-
-    get btnCreateBack() {
-        return $('id:back')
-    }
+    get firstName() { return $('id:firstName') }
+    get lastName() { return $('id:lastName') }
+    get phoneNumber() { return $('id:phone') }
+    get email() { return $('id:email') }
+    get password() { return $('id:password') }
+    get repassword() { return $('id:repassword') }
+    get btnCreate() { return $('id:create') }
+    get btnCreateWishlist() { return $('id:wishlist') }
+    get btnCreateBack() { return $('id:back') }
 
     // 2. Método de Execução
     async createAccount(firstName, lastName, phoneNumber, email, password, repassword) {
-        // CORRIGIDO: mudado de this.signUpButton para this.btnsignUp
+        // PASSO 1: Vai até a tela onde o botão de SignUp existe
+        await this.tabProfile.waitForDisplayed({ timeout: 15000 });
+        await this.tabProfile.click();
+
+        // PASSO 2: Agora sim clica no botão Sign Up
         await this.btnsignUp.waitForDisplayed({ timeout: 15000 });
         await this.btnsignUp.click();
 
+        // PASSO 3: Preenche o formulário
         await this.firstName.waitForDisplayed({ timeout: 15000 });
         await this.firstName.setValue(firstName);
 
@@ -66,7 +49,7 @@ class CadastroPage {
         await this.repassword.waitForDisplayed({ timeout: 10000 });
         await this.repassword.setValue(repassword);
 
-        // Submissão e telas finais
+        // PASSO 4: Finaliza
         await this.btnCreate.waitForDisplayed({ timeout: 10000 });
         await this.btnCreate.click();
 
