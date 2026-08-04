d426418b5a01eeb31f192d16abdecaf7c7a8a563
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2026-08-04 10:38:44 -0300

    subindo atualizaçoes para correção do arquivo workflows .5

--- test/pageobjects/cadastro.page.js
+++ test/pageobjects/cadastro.page.js
@@ -42,7 +42,8 @@ class CadastroPage {
     }
 
     async createAccount(firstName, lastName, phoneNumber, email, password, repassword) {
-        await this.btnsignUp.waitForDisplayed({ timeout: 100000 });
+        await this.signUpButton.waitForDisplayed({ timeout: 15000 });
+        await this.signUpButton.click();
         await this.btnsignUp.click();
         await this.firstName.setValue(firstName);
         await this.lastName.setValue(lastName);
@@ -51,7 +52,7 @@ class CadastroPage {
         await this.password.setValue(password);
         await this.repassword.setValue(repassword);
         await this.btnCreate.click();
-        await this.btnCreateWishlist.waitForDisplayed({ timeout: 100000 });
+        await this.btnCreateWishlist.waitForDisplayed({ timeout: 10000 });
         await this.btnCreateWishlist.click();
         await this.btnCreateBack.click();
     }
