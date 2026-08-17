d219ee8d7e53a4cdd1d44229ae8ef40eb2ee8a00
Pedro001979 <144935572+Pedro001979@users.noreply.github.com>
2026-08-17 16:53:08 -0300

    Estabiliza configuração WebdriverIO Appium no CI

--- wdio.conf.js
+++ wdio.conf.js
@@ -2,13 +2,9 @@ export const config = {
     runner: 'local',
     maxInstances: 1,
 
-    // Conecta no Appium já iniciado na porta 4723
     hostname: '127.0.0.1',
     port: 4723,
     path: '/',
-
-    // REMOVA OU COMENTE O SERVICE:
-    // services: ['appium'], 
 
     specs: [
         './test/specs/Cadastro.test.js'
@@ -18,31 +14,37 @@ export const config = {
         platformName: 'Android',
         'appium:automationName': 'UiAutomator2',
         'appium:deviceName': 'Android Emulator',
+        'appium:platformVersion': '29',
 
         'appium:app': './apps/ebacshop.apk',
         'appium:appPackage': 'br.com.lojaebac',
         'appium:appActivity': '.MainActivity',
 
+        'appium:autoGrantPermissions': true,
         'appium:noReset': false,
         'appium:fullReset': false,
-        'appium:autoGrantPermissions': true,
+        'appium:shouldTerminateApp': true,
 
         'appium:adbExecTimeout': 120000,
         'appium:androidInstallTimeout': 180000,
         'appium:uiautomator2ServerInstallTimeout': 180000,
         'appium:uiautomator2ServerLaunchTimeout': 180000,
+        'appium:newCommandTimeout': 300,
 
-        'appium:newCommandTimeout': 300
+        'appium:disableWindowAnimation': true,
+        'appium:skipServerInstallation': false,
+        'appium:skipDeviceInitialization': false
     }],
 
     logLevel: 'info',
-    waitforTimeout: 15000,
+    waitforTimeout: 60000,
     connectionRetryTimeout: 180000,
     connectionRetryCount: 3,
 
     framework: 'mocha',
+
     mochaOpts: {
         ui: 'bdd',
-        timeout: 120000
+        timeout: 240000
     }
 };
