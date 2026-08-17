d473f6592a7e2f4864d0191e374cca1c7f7196d8
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2026-08-17 17:22:52 -0300

    Simplificar workflow e remover erros de sintaxe

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,40 +22,21 @@ export const config = {
 
         'appium:autoGrantPermissions': true,
         'appium:noReset': false,
-<<<<<<< HEAD
-=======
-        'appium:fullReset': false,
-        'appium:shouldTerminateApp': true,
->>>>>>> abc407296ed363a0cd8b9dd9186e617f6c726d13
 
         'appium:adbExecTimeout': 120000,
         'appium:androidInstallTimeout': 180000,
         'appium:uiautomator2ServerLaunchTimeout': 180000,
-<<<<<<< HEAD
     }],
 
     logLevel: 'info',
     waitforTimeout: 30000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 2,
-=======
-        'appium:newCommandTimeout': 300,
-
-        'appium:disableWindowAnimation': true,
-        'appium:skipServerInstallation': false,
-        'appium:skipDeviceInitialization': false
-    }],
-
-    logLevel: 'info',
-    waitforTimeout: 60000,
-    connectionRetryTimeout: 180000,
-    connectionRetryCount: 3,
->>>>>>> abc407296ed363a0cd8b9dd9186e617f6c726d13
 
     framework: 'mocha',
 
     mochaOpts: {
         ui: 'bdd',
-        timeout: 240000
+        timeout: 120000
     }
 };
