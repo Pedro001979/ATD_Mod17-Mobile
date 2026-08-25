de5fe9a38a013b9e6a23ff62efc018f2644c4a7f
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2026-08-25 13:49:05 -0300

    Corrigindo create

--- test/pageobjects/cadastro.page.js
+++ test/pageobjects/cadastro.page.js
@@ -10,7 +10,9 @@ class CadastroPage {
     get email() { return $('//android.widget.EditText[@resource-id="email"]') }
     get password() { return $('//android.widget.EditText[@resource-id="password"]') }
     get repassword() { return $('//android.widget.EditText[@resource-id="repassword"]') }
-    get btnCreate() { return $('//android.widget.TextView[@text="Create"]') } // busca por texto
+    get createButton() {
+        return $('~Create');
+    }
     // get btnwishlist() { return $('//android.widget.TextView[@resource-id="wishlist"]') } // busca por resourceId
     // get btnback() { return $('//android.widget.TextView[@text=""]') } // busca por content-desc
 
